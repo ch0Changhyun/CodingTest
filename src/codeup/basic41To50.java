241998cc91509bba904a6e46100cc1a8976409ba
chc <311mikecho@gmail.com>
2024-04-19 17:35:55 +0900

    코드업 100제 cont.

---
+++ src/codeup/basic41To50.java
@@ -0,0 +1,89 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class basic41To50 {
+    public static void main(String[] args) {
+//        41
+//        System.out.print("문자 1개 입력 받아 다음 문자 출력 : ");
+//        Scanner scanner = new Scanner(System.in);
+//        char a = scanner.next().charAt(0);
+//        System.out.println((char)(a+1));
+
+//        42
+//        System.out.print("정수 2개 입력 받아 나눈 몫 출력 : ");
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        System.out.println(a / b);
+
+//        43
+//        System.out.print("정수 2개 입력 받아 나눈 나머지 출력 : ");
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        System.out.println(a % b);
+
+//        44
+//        System.out.print("정수 1개 입력 받아 1 더해 출력 : ");
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        System.out.println(a+1);
+
+//        45
+//        System.out.print("정수 2개 입력 받아 -> 자동 계산하기 출력 : ");
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        System.out.println(a+b);
+//        System.out.println(a-b);
+//        System.out.println(a*b);
+//        System.out.println(a/b);
+//        System.out.println(a%b);
+//        double A = (double)a;
+//        System.out.printf("%.2f", A/b);
+
+//        46
+//        System.out.print("정수 3개 입력 받아 -> 합과 평균 출력 : ");
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        int c = scanner.nextInt();
+//        System.out.println(a+b+c);
+//        System.out.println((double)(a+b+c)/3);
+
+//        47
+//        System.out.print("정수 1개 입력 받아 -> 2배 곱해 출력 : ");
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        System.out.printf("%d", a<<1);
+
+//        48
+//        System.out.print("한번에 2의 거듭제곱 배로 출력 : ");
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        System.out.printf("%d", a<<b);
+
+//        49
+//        System.out.print("두 정수 입력 받아 비교하기 : ");
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        if (a>b) {System.out.print(1);
+//        } else {
+//            System.out.print(0);
+//        }
+
+//        50
+        System.out.print("두 정수 입력 받아 비교하기 : ");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        if (a==b) {System.out.print(1);
+        } else {
+            System.out.print(0);
+        }
+    }
+}
+
