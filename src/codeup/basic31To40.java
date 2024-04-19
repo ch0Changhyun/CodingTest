5374e77c2416f13db3dcaad320e537156fb52a18
chc <311mikecho@gmail.com>
2024-04-19 17:04:31 +0900

    코드업 100제 cont.

---
+++ src/codeup/basic31To40.java
@@ -0,0 +1,72 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class basic31To40 {
+    public static void main(String[] args) {
+//        31
+//        System.out.print("10진수 -> 8진수 출력 : ");
+//        Scanner scanner = new Scanner(System.in);
+//        int i = scanner.nextInt();
+//        System.out.printf("%o", i);
+
+//        32
+//        System.out.print("10진수 -> 16진수 출력 : ");
+//        Scanner scanner = new Scanner(System.in);
+//        int i = scanner.nextInt();
+//        System.out.printf("%x", i);
+
+//        33
+//        System.out.print("10진수 -> 16진수 출력 : ");
+//        Scanner scanner = new Scanner(System.in);
+//        int i = scanner.nextInt();
+//        System.out.printf("%X", i);
+
+//        34
+//        System.out.print("8진수 -> 10진수 출력 : ");
+//        Scanner scanner = new Scanner(System.in);
+//        String str = scanner.next();
+//        int octal = Integer.parseInt(str, 8);
+//        System.out.println(octal);
+
+//        35
+//        System.out.print("16진수 -> 8진수 출력 : ");
+//        Scanner scanner = new Scanner(System.in);
+//        String str = scanner.next();
+//        int octal = Integer.parseInt(str, 16);
+//        System.out.printf("%o", octal);
+
+//        36
+//        System.out.print("영문자 1개 입력 -> 아스키 코드표의 10진수 값 출력 : ");
+//        Scanner scanner = new Scanner(System.in);
+//        char c = scanner.next().charAt(0);
+//        System.out.println((int)c);
+
+//        37
+//        System.out.print("정수 1개 입력 -> 아스키 코드표의 문자 출력 : ");
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        System.out.println((char)a);
+
+//        38
+//        System.out.print("정수 2개 입력 -> 합을 출력 : ");
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        System.out.println(a + b);
+
+//        39
+//        System.out.print("정수 2개 입력 -> 합을 출력 : ");
+//        Scanner scanner = new Scanner(System.in);
+//        long a = scanner.nextLong();
+//        long b = scanner.nextLong();
+//        System.out.println(a + b);
+
+//        40
+        System.out.print("입력 된 정수 부호 바꿔서 출력 : ");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println(a * -1);
+    }
+
+}
