f3e46132f534b4646472c7743681156d2f115d2b
ch0Changhyun <98530076+ch0Changhyun@users.noreply.github.com>
2024-11-22 10:26:01 +0900

    [level 1] Title: 하샤드 수, Time: 0.18 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,12 +1,19 @@
 class Solution {
     public boolean solution(int x) {
-        int sum = 0; 
-        int temp = x;
+        // int sum = 0; 
+        // int temp = x;
         
-        while(temp > 0){
-            sum += temp % 10;
-            temp /= 10;
+        // while(temp > 0){
+        //     sum += temp % 10;
+        //     temp /= 10;
+        // }
+        
+        String [] temp = String.valueOf(x).split("");
+        int sum=0;
+        for(String s:temp) {
+            sum+=Integer.parseInt(s);
         }
+    
         return x % sum == 0;
     }
 }
